fd0bb642714b40adec20e79a05b56868cbdfa911
Daniel <drosenzweig06@gmail.com>
2024-02-05 12:25:02 -0800

    ewfrwef

---
+++ Wordel.java
@@ -0,0 +1,28 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.*;
+/**
+ * Write a description of class Worlde here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Wordel implements ActionListener, Runnable
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Worlde
+     */
+    public void run()
+    {
+        
+    }
+
+    public void actionPerformed(ActionEvent event)
+    {
+        
+    }
+}
