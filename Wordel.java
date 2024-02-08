e828484aeffaa1fd1c4a2277fa0ba62d330ced62
Daniel <drosenzweig06@gmail.com>
2024-02-08 08:35:28 -0800

    wadaew

--- Wordel.java
+++ Wordel.java
@@ -3,7 +3,7 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.util.*;
 /**
- * Write a description of class Worlde here.
+ * Write a description of class Wordel here.
  *
  * @author (your name)
  * @version (a version number or a date)
@@ -11,16 +11,37 @@ import java.util.*;
 public class Wordel implements ActionListener, Runnable
 {
     // instance variables - replace the example below with your own
-    private int x;
-
+    JFrame frame;
+    JPanel main;
+    JTextField box;
     /**
      * Constructor for objects of class Worlde
      */
     public void run()
     {
+        frame = new JFrame();
+        frame.setSize(600,800);
+        frame.setLocation(100,100);
+        frame.setTitle("Window Title");
+        frame.setResizable(false);
+        main = new JPanel(new BorderLayout(20, 20));
+        frame.setContentPane(main);
         
+        // Create content panel without a layout manager
+        main = new JPanel();
+        main.setLayout(null);
+        frame.setContentPane(main);
+        for(int i = 0; i < 6; i++) {
+            for (int j = 0; j < 5; j++) {
+                box = new JTextField();
+                box.setSize(40,40);
+                box.setLocation(50 * j,50);
+                main.add(box);
+            }
+        }
+        frame.setVisible(true);
     }
-
+    
     public void actionPerformed(ActionEvent event)
     {
         
