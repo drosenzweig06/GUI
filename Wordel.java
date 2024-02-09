569b382e7fd19d6834b483df3515114edb1b57d6
Daniel <drosenzweig06@gmail.com>
2024-02-09 08:39:17 -0800

    dsfwsegeag

--- Wordel.java
+++ Wordel.java
@@ -8,12 +8,32 @@ import java.util.*;
  * @author (your name)
  * @version (a version number or a date)
  */
-public class Wordel implements ActionListener, Runnable
+public class Wordel implements ActionListener, Runnable, KeyListener
 {
     // instance variables - replace the example below with your own
     JFrame frame;
     JPanel main;
-    JTextField box;
+    JTextField[][] box = new JTextField[7][6];
+    
+    public void keyPressed(KeyEvent e) {
+        char key = e.getKeyChar();
+        if(key == e.CHAR_UNDEFINED) {
+            
+        } else if(key == e.VK_BACK_SPACE) {
+            
+        } else if(key == e.VK_EQUALS) {
+            
+        }
+    }
+    
+    public void keyReleased(KeyEvent e) {
+        
+    }
+    
+    public void keyTyped(KeyEvent e) {
+
+    }
+    
     /**
      * Constructor for objects of class Worlde
      */
@@ -26,17 +46,18 @@ public class Wordel implements ActionListener, Runnable
         frame.setResizable(false);
         main = new JPanel(new BorderLayout(20, 20));
         frame.setContentPane(main);
+        frame.addKeyListener(this);
         
         // Create content panel without a layout manager
         main = new JPanel();
         main.setLayout(null);
         frame.setContentPane(main);
-        for(int i = 0; i < 6; i++) {
-            for (int j = 0; j < 5; j++) {
-                box = new JTextField();
-                box.setSize(40,40);
-                box.setLocation(50 * j,50);
-                main.add(box);
+        for(int i = 0; i < 5; i++) {
+            for (int j = 0; j < 6; j++) {
+                box[i][j] = new JTextField();
+                box[i][j].setSize(40,40);
+                box[i][j].setLocation(50 + (i*50) ,50 + (j*50) );
+                main.add(box[i][j]);
             }
         }
         frame.setVisible(true);
