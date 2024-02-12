efc65f8148114d42c87ee8aab910720203a5f4c9
Daniel <drosenzweig06@gmail.com>
2024-02-12 13:16:34 -0800

    edrrteh

--- Wordle.java
+++ Wordle.java
@@ -8,21 +8,26 @@ import java.util.*;
  * @author (your name)
  * @version (a version number or a date)
  */
-public class Wordel implements ActionListener, Runnable, KeyListener
+public class Wordle implements ActionListener, Runnable, KeyListener
 {
     // instance variables - replace the example below with your own
     JFrame frame;
     JPanel main;
     JTextField[][] box = new JTextField[7][6];
+    int countX = 0;
+    int countY = 0;
     
     public void keyPressed(KeyEvent e) {
         char key = e.getKeyChar();
-        if(key == e.CHAR_UNDEFINED) {
-            
-        } else if(key == e.VK_BACK_SPACE) {
-            
-        } else if(key == e.VK_EQUALS) {
-            
+        if (key != e.CHAR_UNDEFINED && Character.isLetter(key)) {
+            placeCharacter(key);
+        } else if (e.getKeyCode() == e.VK_BACK_SPACE &&
+                    countX <= 5 && countX >= 1) {
+            countX--;
+            box[countX][countY].setText("");
+        } else if (e.getKeyCode() == e.VK_ENTER && countX == 5) {
+            countX = 0;
+            countY++;
         }
     }
     
@@ -32,6 +37,13 @@ public class Wordel implements ActionListener, Runnable, KeyListener
     
     public void keyTyped(KeyEvent e) {
 
+    }
+    
+    public void placeCharacter(char k) {
+        if(countX < 5) {
+            box[countX][countY].setText(k + "");
+            countX++;
+        }
     }
     
     /**
@@ -46,7 +58,7 @@ public class Wordel implements ActionListener, Runnable, KeyListener
         frame.setResizable(false);
         main = new JPanel(new BorderLayout(20, 20));
         frame.setContentPane(main);
-        frame.addKeyListener(this);
+        
         
         // Create content panel without a layout manager
         main = new JPanel();
@@ -55,16 +67,27 @@ public class Wordel implements ActionListener, Runnable, KeyListener
         for(int i = 0; i < 5; i++) {
             for (int j = 0; j < 6; j++) {
                 box[i][j] = new JTextField();
+                box[i][j].setEditable(false);
                 box[i][j].setSize(40,40);
-                box[i][j].setLocation(50 + (i*50) ,50 + (j*50) );
+                box[i][j].setLocation(50 + (i*50) ,50 + (j*50));
+                box[i][j].addKeyListener(this);
                 main.add(box[i][j]);
             }
         }
+        frame.addKeyListener(this);
         frame.setVisible(true);
     }
     
     public void actionPerformed(ActionEvent event)
     {
         
+    }
+    
+    public void checkWord() {
+        
+    }
+    
+    public static void start() {
+       SwingUtilities.invokeLater(new Wordle());
     }
 }
